469036828e9bff016c027e85b1bfa62096eea00b
Ryan Duff <ryanduff613@gmail.com>
2020-07-20 15:32:35 -0700

    move application state into redux store

--- src/__tests__/reducers/headlines-reducer.test.js
+++ src/__tests__/reducers/headlines-reducer.test.js
@@ -11,6 +11,28 @@ describe('headlinesReducer', () => {
     error: null,
   };
 
+  const loadingState = {
+    isLoading: false,
+    headlines: [],
+    error: null
+  };
+
+  //  ====== GET_HEADLINES_SUCCESS ======
+  test('succesfully getting headlines should change isLoading to false and update headlines', () => {
+    const headlines = "a headline";
+    action = {
+      type: c.GET_HEADLINES_SUCCESS,
+      headlines
+    }
+
+    expect(headlinesReducer(loadingState, action)).toEqual({
+      isLoading: false,
+      headlines: 'a headline',
+      error: null
+    });
+  });
+  
+  //  ====== REQUEST_HEADLINES ======
   test('requesting headlines should successfully change isLoading from false to true', () => {
     action = {
       type: c.REQUEST_HEADLINES
@@ -23,6 +45,7 @@ describe('headlinesReducer', () => {
     })
   });
 
+  // ====== DEFAULT_STATE ======
   test('should return default state if no action passed', () => {
     expect(headlinesReducer(defaultState, {type: null})).toEqual(
       {
@@ -31,4 +54,20 @@ describe('headlinesReducer', () => {
         error: null
     })
   });
+
+  //  ======  GET_HEADLINES_FAILURE ======
+  test('failing to get headlines should change isLoading to false and add an error message', () => {
+    const error = "An error";
+    action = {
+      type: c.GET_HEADLINES_FAILURE,
+      error
+    };
+
+    expect(headlinesReducer(loadingState, action)).toEqual({
+      isLoading: false,
+      headlines: [],
+      error: "An error"
+    });
+  });
+
 });
